f8ac97017467c1346f8417665eba833e18b7cff9
ngautam0 <ngautam0@live.com>
2016-03-27 01:14:27 +0530

    Changes made for ng-view

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,19 +1,21 @@
-var app = angular.module('altizonApp',[]);
+var app = angular.module('altizonApp',['ngRoute','ngAnimate']);
 
 app.controller('altizonAppCtrl',[
   '$scope',
-  '$http',
-  '$parse',
+'$http',
+'$location',
 
-  function($scope,$http) {
+  function($scope,$http,$location) {
   console.log("hello @4");
 
   $scope.credentialsSubmitted=function(){
+    $scope.showLoginFailed=false;
 
       if (!$scope.username || $scope.username === ''|| !$scope.password || $scope.password === '' ) {
         return;
       }
       else{
+
         // alert($scope.username);
         // alert($scope.password);
         var userAndPass={
@@ -25,7 +27,26 @@ app.controller('altizonAppCtrl',[
         success(function(data, status, headers, config) {
           console.log(data);
           $scope.showLoadingScreen=false;
+          if (data["error"] !== undefined) {
+            $scope.showLoginFailed=true;
+          }
+          else {
+            console.log("in else part");
+            $location.path('/dashboards');
+          }
         });
       }
   }
+}]);
+
+
+
+
+app.controller('dashboardsCtrl',[
+  '$scope',
+  '$http',
+  '$location',
+
+  function($scope,$http) {
+
 }]);
---
+++ public/javascripts/service.js
@@ -0,0 +1,17 @@
+var application=angular.module('altizonApp', ['ngRoute']);
+console.log("1 in route provider");
+
+application.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    console.log("2 in route provider");
+    $routeProvider.
+      when('/', {
+        templateUrl: './login.html',
+        controller: 'altizonAppCtrl'
+      })
+      .when('/dashboards',{
+        templateUrl: './dashboards.html',
+        controller: 'dashboardsCtrl'
+      })
+      .otherwise({redirectTo: '/'});
+  }]);
+console.log("3 in route provider");
